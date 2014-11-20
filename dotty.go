e232af59b48f077939d868b11b8555ea0a828767
deoxxa <deoxxa@fknsrs.biz>
2014-11-20 21:02:37 +1100

    use reflection, apparently

--- dotty.go
+++ dotty.go
@@ -2,6 +2,8 @@ package dotty
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -13,31 +15,27 @@ func Exists(src interface{}, key string) (exists bool, err error) {
 
 	value := src
 	for _, bit := range bits {
-		switch value.(type) {
-		case map[string]interface{}:
-			_, ok := value.(map[string]interface{})[bit]
-			if ok != true {
+		_type := reflect.TypeOf(value)
+
+		if _type.Kind() == reflect.Map && _type.Key().Kind() == reflect.String {
+			if currentValue := reflect.ValueOf(value).MapIndex(reflect.ValueOf(bit)); currentValue == reflect.Zero(_type) {
 				exists = false
 				return
+			} else {
+				value = currentValue.Interface()
 			}
-
-			value = value.(map[string]interface{})[bit]
-		case []interface{}:
-			i, ierr := strconv.ParseInt(bit, 10, 32)
-			if ierr != nil {
-				err = ierr
+		} else if _type.Kind() == reflect.Array {
+			if i, _err := strconv.ParseInt(bit, 10, 32); _err != nil {
+				err = _err
 				return
-			}
-
-			if len(value.([]interface{})) <= int(i) {
+			} else if reflect.ValueOf(value).Len() <= int(i) {
 				exists = false
 				return
+			} else {
+				value = reflect.ValueOf(value).Index(int(i)).Interface()
 			}
-
-			value = value.([]interface{})[i]
-		default:
-			err = errors.New("encountered invalid type in object")
-			return
+		} else {
+			err = errors.New(fmt.Sprintf("Exists() encountered invalid type (%s) in object (at %s's parent)", _type, bit))
 		}
 	}
 
@@ -49,18 +47,19 @@ func Get(src interface{}, key string) (value interface{}, err error) {
 
 	value = src
 	for _, bit := range bits {
-		switch value.(type) {
-		case map[string]interface{}:
-			value = value.(map[string]interface{})[bit]
-		case []interface{}:
-			i, ierr := strconv.ParseInt(bit, 10, 32)
-			if ierr != nil {
-				err = ierr
+		_type := reflect.TypeOf(value)
+
+		if _type.Kind() == reflect.Map && _type.Key().Kind() == reflect.String {
+			value = reflect.ValueOf(value).MapIndex(reflect.ValueOf(bit)).Interface()
+		} else if _type.Kind() == reflect.Array {
+			if i, _err := strconv.ParseInt(bit, 10, 32); _err != nil {
+				err = _err
 				return
+			} else {
+				value = reflect.ValueOf(value).Index(int(i)).Interface()
 			}
-			value = value.([]interface{})[i]
-		default:
-			err = errors.New("encountered invalid type in object")
+		} else {
+			err = errors.New(fmt.Sprintf("Get() encountered invalid type (%s) in object (at %s's parent)", _type, bit))
 			return
 		}
 	}
@@ -72,44 +71,47 @@ func Put(dst interface{}, key string, value interface{}) (err error) {
 	bits := strings.Split(key, ".")
 
 	target := dst
-	for i, bit := range bits {
-		if i == len(bits)-1 {
-			switch target.(type) {
-			case map[string]interface{}:
-				target.(map[string]interface{})[bit] = value
-				err = nil
+	for _, bit := range bits[0 : len(bits)-1] {
+		_type := reflect.TypeOf(target)
+		_value := reflect.ValueOf(target)
+
+		if _type == nil {
+			err = errors.New(fmt.Sprintf("Put() encountered nil value in object (at %s's parent)", bit))
+			return
+		} else if _type.Kind() == reflect.Map && _type.Key().Kind() == reflect.String {
+			if _target := reflect.ValueOf(value).MapIndex(reflect.ValueOf(bit)); _target == reflect.Zero(_type) {
+				target = map[string]interface{}{}
+				_value.SetMapIndex(reflect.ValueOf(bit), reflect.ValueOf(target))
+			} else {
+				target = _target.Interface()
+			}
+		} else if _type.Kind() == reflect.Array || _type.Kind() == reflect.Slice {
+			if n, _err := strconv.ParseInt(bit, 10, 32); _err != nil {
+				err = _err
 				return
-			default:
-				err = errors.New("final object target was not a map")
-			}
-		}
-
-		switch target.(type) {
-		case map[string]interface{}:
-			_, ok := target.(map[string]interface{})[bit]
-			if ok != true {
-				target.(map[string]interface{})[bit] = map[string]interface{}{}
-			}
-
-			target = target.(map[string]interface{})[bit]
-		case []interface{}:
-			n, nerr := strconv.ParseInt(bit, 10, 32)
-			if nerr != nil {
-				err = nerr
+			} else if _value.Len() <= int(n) {
+				err = errors.New(fmt.Sprintf("Put() encountered an array that was shorter (%d) than the key required (%d)", _value.Len(), int(n)))
 				return
+			} else {
+				target = _value.Index(int(n))
 			}
-
-			if len(target.([]interface{})) <= int(n) {
-				err = errors.New("encountered an array that was shorter than the key required")
-				return
-			}
-
-			target = target.([]interface{})[n]
-		default:
-			err = errors.New("encountered invalid type in object")
+		} else {
+			err = errors.New(fmt.Sprintf("Put() encountered invalid type (%s) in object (at %s's parent)", _type, bit))
 			return
 		}
 	}
+
+	_type := reflect.TypeOf(target)
+	_value := reflect.ValueOf(target)
+
+	if _type == nil || _type.Kind() != reflect.Map || _type.Key().Kind() != reflect.String {
+		err = errors.New(fmt.Sprintf("Put() target key (%s)'s parent was not a map (%s)", key, _type))
+		return
+	}
+
+	bit := bits[len(bits)-1]
+
+	_value.SetMapIndex(reflect.ValueOf(bit), reflect.ValueOf(value))
 
 	return
 }
